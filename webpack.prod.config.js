6b7f3ee0e9eada7bd8927fb41d600b43ddfb408a
Ivo Branco <ivo.branco@fccn.pt>
2024-12-18 15:27:23 +0000

    fix: build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,17 +4,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const config = createConfig('webpack-prod');
 
-config.plugins.push(
-  new CopyPlugin({
-    patterns: [
-      {
-        from: path.resolve(__dirname, './public/static'),
-        to: path.resolve(__dirname, './dist/static'),
-      },
-    ],
-  }),
-);
-
 config.resolve.alias = {
   ...config.resolve.alias,
   '@src': path.resolve(__dirname, 'src'),
