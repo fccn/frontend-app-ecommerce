4dd32c97896b165279b368a78f10f57f59f5259c
Brayan Ceron <brayan.ceron@edunext.co>
2025-12-04 09:13:25 -0500

    chore: update environment configuration and fix module resolution in webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,4 +9,12 @@ config.resolve.alias = {
   '@src': path.resolve(__dirname, 'src'),
 };
 
+// Fix for react-focus-on module resolution issue in webpack 5
+config.module.rules.push({
+  test: /\.m?js$/,
+  resolve: {
+    fullySpecified: false,
+  },
+});
+
 module.exports = config;
